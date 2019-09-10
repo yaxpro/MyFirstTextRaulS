bfac8c6b1aef268457f264faf44ddb9a87c3f11a
LAB15 <raul.silca@gmail.com>
2019-09-10 09:46:06 -0300

    Initial commit

---
+++ app/src/main/java/com/example/myfirsttextrauls/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.myfirsttextrauls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Este es mi Toast simple", Toast.LENGTH_LONG).show();
+    }
+}
